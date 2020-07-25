7fd3f3e96c51713a05bed5a1b2f822eaabcd3d5a
Кирилл <kkirillcore@gmail.com>
2020-07-25 19:24:18 +0300

    Update 2020-07-25T16:24:11.988Z

--- source/js/mask.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-$(document).ready(function () {
-  $('#request-number').mask('+7(000)-000-00-00');
-  $('#promotion-phone').mask('+7(000)-000-00-00');
-  $('#consultation-phone').mask('+7(000)-000-00-00');
-});
