95dde3823db95f0169ba39c039623ce48d8853df
Кирилл <kkirillcore@gmail.com>
2020-07-25 19:13:21 +0300

    доделывает проект

---
+++ build/js/mask.js
@@ -0,0 +1,7 @@
+'use strict';
+
+$(document).ready(function () {
+  $('#request-number').mask('+7(000)-000-00-00');
+  $('#promotion-phone').mask('+7(000)-000-00-00');
+  $('#consultation-phone').mask('+7(000)-000-00-00');
+});
